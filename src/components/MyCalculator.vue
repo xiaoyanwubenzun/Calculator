bc64c61be9c185ac1ee7b658a875b69694f7534f
xiaoyanwu <1769122103@qq.com>
2021-11-08 14:03:34 +0800

    refactor(1.1版本提交): 1.1版本提交
    1.1版本提交

--- src/components/MyCalculator.vue
+++ src/components/MyCalculator.vue
@@ -4,15 +4,13 @@
 			<div class="show-calculator-operation">
 				<span 
 					class="operation-choose" 
-					v-cloak
 				>
-				{{ previous_calculation_init }}{{ operation_choose }}
+				{{ `${previous_calculation_init}${operation_choose}` }}
 				</span>
 			</div>
 			<div class="show-calculator-result">
 				<span 
 					class="calculation-init" 
-					v-cloak
 				>
 				{{ calculation_init || 0 }}
 				</span>
@@ -45,171 +43,18 @@
 				</button>
 				<button>M^</button>
 			</div>
-			<div class="calculator-standard">
+			<div 
+				class="calculator-standard" 
+				@click="getBtnClick"
+			>
 				<div>
 					<button
-						class="calculator-btn-conversion"
-						:disabled="btn_disabled"
-						@click="persent()"
+						v-for="(item,index) in all_btn" 
+						:key="index"
+						:disabled="btnDisabled(item)"
+						:class="btnStyle(item)"
 					>
-						%
-					</button>
-					<button
-						class="calculator-btn-conversion"
-						@click="reset()"
-					>
-						CE
-					</button>
-					<button
-						class="calculator-btn-conversion"
-						@click="clear()"
-					>
-						C
-					</button>
-					<button
-						class="calculator-btn-conversion"
-						@click="deleteInputnum()"
-					>
-						DEL
-					</button>
-				</div>
-				<div>
-					<button
-						class="calculator-btn-conversion"
-						:disabled="btn_disabled"
-						@click="reciprocal()"
-					>
-						1/x
-					</button>
-					<button
-						class="calculator-btn-conversion"
-						:disabled="btn_disabled"
-						@click="square()"
-					>
-						x^2
-					</button>
-					<button
-						class="calculator-btn-conversion"
-						:disabled="btn_disabled"
-						@click="sqrt()"
-					>
-						√
-					</button>
-					<button
-						class="operation-btn-style"
-						:disabled="btn_disabled"
-						@click="divide()"
-					>
-						÷
-					</button>
-				</div>
-				<div>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(7)"
-					>
-						7
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(8)"
-					>
-						8
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(9)"
-					>
-						9
-					</button>
-					<button
-						class="operation-btn-style"
-						:disabled="btn_disabled"
-						@click="multiply()"
-					>
-						×
-					</button>
-				</div>
-				<div>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(4)"
-					>
-						4
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(5)"
-					>
-						5
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(6)"
-					>
-						6
-					</button>
-					<button
-						class="operation-btn-style"
-						:disabled="btn_disabled"
-						@click="subtract()"
-					>
-						-
-					</button>
-				</div>
-				<div>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(1)"
-					>
-						1
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(2)"
-					>
-						2
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(3)"
-					>
-						3
-					</button>
-					<button
-						class="operation-btn-style"
-						:disabled="btn_disabled"
-						@click="add()"
-					>
-						+
-					</button>
-				</div>
-				<div>
-					<button
-						class="calculator-btn-conversion"
-						:disabled="btn_disabled"
-						@click="takeNegative()"
-					>
-						+/-
-					</button>
-					<button
-						class="calculator-btn-operation"
-						@click="inputNum(0)"
-					>
-						0
-					</button>
-					<button
-						class="calculator-btn-conversion"
-						:disabled="btn_disabled"
-						@click="addPoint()"
-					>
-						.
-					</button>
-					<button
-						class="equals operation-btn-style"
-						@click="equal()"
-					>
-						=
+						{{item}}
 					</button>
 				</div>
 			</div>
@@ -218,39 +63,92 @@
 </template>
 
 <script>
+	const ALL_BTN = Object.freeze(["%","CE","C","DEL","1/x","x^2","√","÷","7","8","9","×","4","5","6","-","1","2","3","+","+/-","0",".","="]);
+	const INPUT_NUM_ARR = Object.freeze([1,2,3,4,5,6,7,8,9,0]);
+	const SELF_OPERATION_ARR = Object.freeze(["%","CE","C","DEL","1/x","x^2","√","+/-"]);
+	const OPERATION_ARR = Object.freeze(["÷","×","-","+"]);
+	const SELF_OPERATION_METHOD_SWITCH = Object.freeze({
+					"%": "percent",
+					"CE": "clearResult",
+					"C": "clearShow",
+					"DEL": "deleteInputNum",
+					"1/x": "reciprocal",
+					"x^2": "square",
+					"√": "sqrt",
+					"+/-": "takeNegative"
+				});
+	const OPERATION_METHOD_SWITCH = Object.freeze({
+					"÷": "divide",
+					"×": "multiply",
+					"-": "subtract",
+					"+": "add",
+				});
 	export default {
 		name: "MyCalculator",
 		data() {
 			return {
+				all_btn: ALL_BTN,
 				calculation_init: "",
 				previous_calculation_init: "",
-				operation_choose: null,
+				operation_choose: "",
 				operation: false,
 				btn_disabled: false,
-				operation_choose_switch: {
-					"÷": "/",
-					"×": "*",
-					"-": "-",
-					"+": "+"
-				}
+				input_num_arr: INPUT_NUM_ARR,
+				self_operation_arr: SELF_OPERATION_ARR,
+				operation_arr: OPERATION_ARR,
+				self_operation_method_switch: SELF_OPERATION_METHOD_SWITCH,
+				operation_method_switch: OPERATION_METHOD_SWITCH
 			};
 		},
+		watch: {
+			calculation_init() {
+				if(this.calculation_init.toString().indexOf(".") === -1) {
+					this.calculation_init = this.calculation_init.toString().replace(/\b(0+)/ig,"");
+				}
+			},
+
+			previous_calculation_init() {
+				if(this.previous_calculation_init === "除数不能为零") {
+					this.previous_calculation_init = "";
+				}
+			}
+		},
 		methods: {
+			// 计算按钮是否可更改禁用属性
+			btnDisabled(item) {
+				return ["%","1/x","x^2","√","÷","×","-","+","+/-","."].indexOf(item) > -1 ? this.btn_disabled : false;
+			},
+
+			// 计算按钮对应样式
+			btnStyle(item) {
+				return `${item === "=" ? "equals" : ""} ${[".","÷","×","-","+","="].indexOf(item) > -1 ? "operation-btn-style" : ""} ${this.self_operation_arr.concat([".","÷","×","-","+"]).indexOf(item) > -1 ? "calculator-btn-conversion" : "calculator-btn-operation"}`;
+			},
+
+			// 判断是否为数字
+			isNumber(value) {
+				return typeof value === "number" && !isNaN(value);
+			},
+
 			// 输入要进行计算的内容
 			inputNum(num) {
-				this.calculation_init = this.calculation_init === "除数不能为0" || this.calculation_init === "无效输入" ||this.operation ? (this.calculation_init = num) : `${this.calculation_init}${num}`;
-				this.btn_disabled = false;
-				this.operation = false;
+				// 非数字或已选择计算方式，重新输入
+				if (!this.isNumber(Number(this.calculation_init)) || this.operation) {
+					this.calculation_init = num;
+					this.btn_disabled = false;
+					this.operation = false;
+				}
+				else {
+					this.calculation_init = `${this.calculation_init}${num}`;
+				}
 			},
 
 			// 百分号计算百分数
-			persent() {
+			percent() {
 				this.calculation_init = this.calculation_init / 100;
-				this.operation_choose = "%";
 			},
 
 			// 只重置结果为""
-			reset() {
+			clearResult() {
 				this.calculation_init = "";
 				this.previous_calculation_init = "";
 				this.operation_choose = "CE";
@@ -258,7 +156,7 @@
 			},
 
 			// C将结果重置为"",将上次点击的运算清除
-			clear() {
+			clearShow() {
 				this.calculation_init = "";
 				this.previous_calculation_init = "";
 				this.operation_choose = "";
@@ -266,9 +164,11 @@
 			},
 
 			// 删除，将字符串从最后一位一位删除
-			deleteInputnum() {
-				if (this.calculation_init === "除数不能为0" || this.calculation_init === "无效输入") {
+			deleteInputNum() {
+				//非数字直接重置
+				if (!this.isNumber(Number(this.calculation_init))) {
 					this.calculation_init = "";
+					this.btn_disabled = false;
 					return;
 				}
 				this.calculation_init = this.calculation_init.toString().slice(0,this.calculation_init.toString().length - 1);
@@ -278,9 +178,8 @@
 
 			// 求倒数
 			reciprocal() {
-				this.operation_choose = "1/x";
 				if (this.calculation_init === "0" || this.calculation_init === "") {
-					this.calculation_init = "除数不能为0";
+					this.calculation_init = "除数不能为零";
 					this.btn_disabled = true;
 					return;
 				}
@@ -289,59 +188,57 @@
 
 			// 平方运算
 			square() {
-				this.operation_choose = "x^2";
 				this.calculation_init = this.calculation_init * this.calculation_init;
 			},
 
 			// 开平方
 			sqrt() {
-				this.operation_choose = "√";
 				// 要进行开根号的数不能为负数
 				if (parseFloat(this.calculation_init) < 0) {
 					this.calculation_init = "无效输入";
 					this.btn_disabled = true;
-				} else {
-					this.calculation_init = Math.sqrt(this.calculation_init);
-				}
+					return
+				} 
+				this.calculation_init = Math.sqrt(this.calculation_init);
 			},
 
 			// 保存calculation_init
-			saveCalculationinit() {
+			saveCalculationInit() {
 				this.previous_calculation_init = this.calculation_init;
 				this.operation = true;
 			},
 
+			// 改变operation_choose
+			changeOperationChoose(operation_choose) {
+				this.operation_choose = operation_choose;
+			},
+
 			// 除
 			divide() {
-				this.equal();
-				this.operation_choose = "÷";
-				this.saveCalculationinit();
+				if (this.calculation_init === "0" || this.calculation_init === "") {
+					this.btn_disabled = true;
+					return "除数不能为零";
+				}
+				return this.previous_calculation_init / this.calculation_init;
 			},
 
 			// 乘
 			multiply() {
-				this.equal();
-				this.operation_choose = "×";
-				this.saveCalculationinit();
+				return this.previous_calculation_init * this.calculation_init;
 			},
 
 			// 减
 			subtract() {
-				this.equal();
-				this.operation_choose = "-";
-				this.saveCalculationinit();
+				return this.previous_calculation_init - this.calculation_init;
 			},
 
 			// 加
 			add() {
-				this.equal();
-				this.operation_choose = "+";
-				this.saveCalculationinit();
+				return Number(this.previous_calculation_init) + Number(this.calculation_init);
 			},
 
 			// 添加或除去负号
 			takeNegative() {
-				this.operation_choose = "+/-";
 				this.calculation_init = `${-this.calculation_init}`;
 			},
 
@@ -349,32 +246,43 @@
 			addPoint() {
 				if (this.calculation_init.toString().indexOf(".") === -1) {
 					this.calculation_init = `${this.calculation_init === "" ? (this.calculation_init = 0) : this.calculation_init}.`;
-				} else if (this.calculation_init.toString().indexOf(".") === -1 && this.calculation_init === "") {
+				} 
+				else if (this.calculation_init.toString().indexOf(".") === -1 && this.calculation_init === "") {
 					this.calculation_init = 0;
 				}
 			},
 
 			// 计算结果
 			equal() {
-				switch (this.operation_choose) {
-				case "÷":
-					this.calculation_init = this.previous_calculation_init / this.calculation_init;
-					break;
-				case "×":
-					this.calculation_init = this.previous_calculation_init * this.calculation_init;
-					break;
-				case "-":
-					this.calculation_init = this.previous_calculation_init - this.calculation_init;
-					break;
-				case "+":
-					this.calculation_init = Number(this.previous_calculation_init) + Number(this.calculation_init);
-					break;
-				default:
-					break;
+				if(!this.operation_choose || this.self_operation_arr.indexOf(this.operation_choose) > -1){
+					return
 				}
-				// console.log(this.operation_choose_switch[`${this.operation_choose}`]);
-				// this.calculation_init = this.previous_calculation_init this.operation_choose_switch[this.operation_choose] this.calculation_init;
-				this.operation_choose = "";
+				this.calculation_init = this[this.operation_method_switch[this.operation_choose]]();
+				this.operation = false;
+			},
+
+			// 获取点击的按钮
+			getBtnClick(event){
+				const btn_clicked = event.target.innerText;
+				if (this.input_num_arr.indexOf(Number(btn_clicked)) > -1) {
+					this.inputNum(Number(btn_clicked));
+				}
+				else if (this.self_operation_arr.indexOf(btn_clicked) > -1){
+					this.changeOperationChoose(btn_clicked);
+					this[this.self_operation_method_switch[`${btn_clicked}`]]();
+				}
+				else if (this.operation_arr.indexOf(btn_clicked) > -1) {
+					this.equal();
+					this.saveCalculationInit();
+					this.changeOperationChoose(btn_clicked);
+				}
+				else if (btn_clicked === "=") {
+					this.equal();
+					this.operation_choose = "";
+				}
+				else if (btn_clicked === ".") {
+					this.addPoint();
+				}
 			},
 		},
 	};
@@ -382,15 +290,11 @@
 
 <style scoped>
 	.content {
-		height: 100vh;
+		height: 98vh;
 		width: 100%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-	}
-
-	[v-cloak] {
-		display: none;
 	}
 
 	.calculator {
@@ -436,14 +340,16 @@
 		line-height: 100px;
 	}
 
-	.show-calculator-operation,.show-calculator-result{
+	.show-calculator-operation,
+	.show-calculator-result{
 		text-align: right;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 
 	.operation-choose,
 	.calculation-init {
 		white-space: nowrap;
-		overflow: auto;
 		max-width: -webkit-fill-available;
 	}
 
@@ -468,6 +374,7 @@
 	.operation-btn-style {
 		font-weight: 300;
 		font-size: 25px;
+		font-family: auto;
 		background-color: #f5f5f5;
 	}
 
